956d531c97ed047c1ce356beff17bd6716ab643b
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2020-12-05 04:42:02 +0100

    #2 Remove comments

--- learning_script.py
+++ learning_script.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-    Luźne notatki
-
-- blastn/eval10/best_hsp_bitscore_mean_normalization
-- blastn/eval10/best_hsp_bitscore_mean_normalization_n50
-- blastn/eval10/best_hsp_bitscore_min_max_normalization_n100
-  - co to n50, n100?
-  - różnica między mean a min-max
-  - problem normalizacji (wartości znormalizowane i nieznormalizowane)
-
-    
-
-  Blastp eval - do rozkmininenia
-
-"""
-
-
 from datetime import datetime
 import json
 from pathlib import Path
@@ -28,7 +11,6 @@ from virusDL import FEATURE_LIST, CPU_COUNT_LOCAL
 from virusDL.DataManager import DataLoader
 from virusDL.models.SimpleDeepLearningModel import SimpleDeepLearningModel
 from virusDL.ModelManager import ModelManager
-
 
 # extractor = DataExtractor(data_folder="data", tests_folder="tests", feature_list=FEATURE_LIST)
 # cpu_count = int(os.environ.get("SLURM_JOB_CPUS_PER_NODE", CPU_COUNT_LOCAL))
@@ -49,13 +31,3 @@ for file in results:
     result_dict.update(json.load(open(file)))
 
 json.dump(result_dict, open('whole.json','w'))
-
-"""wszystkie hosty z danym wirusem
-wyciągam pary i przewiduje
-roc
-precision treccall curve
-
-
-wirusy podobne do siebie
-randomwoy split nie daje generalizacji 
-"""
