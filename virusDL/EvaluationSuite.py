6d70f9e51282b1936186196365a7eefc8da85624
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2020-12-21 19:14:30 +0100

    Add host sorted info evaluation

--- virusDL/EvaluationSuite.py
+++ virusDL/EvaluationSuite.py
@@ -1,12 +1,15 @@
 from pathlib import Path
-import ray
 from pprint import pprint as pp
+
+import ray
+
 from evaluation.evaluate0_create_result_json import concat_and_sort_results
 from evaluation.evaluate1_hosts_sorted_to_hosts_best import filter_best_hosts
 from evaluation.evaluate2_hits_best_json import evaluate_taxonomy_performance
 from evaluation.evaluate3_true_host_rank_distribution import true_host_rank_distribution
 from evaluation.evaluate4_positive_negative import positive_negative_scores_distribution
 from evaluation.evaluate8_threshold_sens_spec import evaluate_binary_classification
+from evaluation.evaluate9_null_value import evaluate_sorted_hosts
 
 ray.init(num_cpus=2)
 tests_folder = Path('/home/panda/workspace/wirusy/tests')
@@ -18,6 +21,7 @@ host_sorted = concat_and_sort_results.remote(tests_folder)
 # host_rank_distribution, rank_distribution_plot = true_host_rank_distribution.remote(host_sorted)
 # scores_distribution, scores_plot = positive_negative_scores_distribution.remote(host_sorted)
 model_evaluation_stats = evaluate_binary_classification.remote(host_sorted)
+# host_sorted_info = evaluate_sorted_hosts.remote(host_sorted)
 
 pp(ray.get(model_evaluation_stats))
 # do jednego katalogu results z datą
--- virusDL/evaluation/evaluate9_null_value.py
+++ virusDL/evaluation/evaluate9_null_value.py
@@ -1,31 +1,12 @@
-import argparse
-import json
-from pathlib import Path
-
 import ray
 
-
-
-parser = argparse.ArgumentParser(description='Lorem ipsum')
-parser.add_argument('-j', '--j', '--json', dest='json', type=argparse.FileType('r'),
-                    help='hits_sorted.json', required=True)
-args = parser.parse_args()
-
-
 @ray.remote
-def run():
-    d = json.load(args.json)
-    args.json.close()
-
-
-    in_json_path = Path(args.json.name)
-    output_path = in_json_path.parent
-
+def evaluate_sorted_hosts(host_sorted):
 
     # Check whether values are scores or distance
     distance = True
-    for vid, hits in d.items():
-        values = [score for hid, score in hits]
+    for hits in host_sorted.values():
+        values = [score for _, score in hits]
         if len(values) > 1:
             if values[0] < values[-1]:
                 break
@@ -33,8 +14,7 @@ def run():
                 distance = False
                 break
 
-
-    lst = [score for vid, hits in d.items() for hid, score in hits]
+    lst = [score for hits in host_sorted.values() for _, score in hits]
     missing_values = 20897286 - len(lst)
 
     min_val = min(lst)
@@ -44,6 +24,7 @@ def run():
     worst_value = max_val if distance else min_val
     null_value = max_val if distance else 0.0
 
+    # TODO: Check this output path
     run_name = str(output_path)
     if 'pvalue' in run_name:
         null_value = 1.0
@@ -70,7 +51,7 @@ def run():
     if 'wish' in run_name:
         null_value = worst_value
 
-    d = {
+    host_sorted_info = {
         'is_distance': distance,
         'missing_values': missing_values,
         'best_value_observed': best_value,
@@ -78,9 +59,4 @@ def run():
         'null_value': null_value 
     }
 
-
-    oh = open(output_path.joinpath('hosts_sorted.info.json'), 'w')
-    json.dump(d, oh, indent=3)
-    oh.close()
-
-run()
+    return host_sorted_info
